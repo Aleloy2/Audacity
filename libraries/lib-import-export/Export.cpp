e26850d18e36e1036c9580aa53de59e176207c48
Aleloy2 <163466397+Aleloy2@users.noreply.github.com>
2024-04-23 19:26:32 -0400

    got it compiled

--- libraries/lib-import-export/Export.cpp
+++ libraries/lib-import-export/Export.cpp
@@ -14,7 +14,7 @@
 *//*****************************************************************/
 
 #include "Export.h"
-
+#include "../../lib-crypto/crypto/SHA256.h"
 #include <numeric>
 
 #include "BasicUI.h"
@@ -24,7 +24,8 @@
 #include "WaveTrack.h"
 #include "wxFileNameWrapper.h"
 #include "StretchingSequence.h"
-#include <openssl/md5.h>
+
+#include "crypto/SHA256.h"
 
 #include "ExportUtils.h"
 
@@ -124,32 +125,20 @@ ExportTask ExportTaskBuilder::Build(AudacityProject& project)
                      targetFilename.GetFullPath(),
                      true);
                }
-unsigned char result[MD5_DIGEST_LENGTH];
-FILE* file = fopen(actualFilename.GetFullPath().c_str(), "rb");
-if (file){
-   unsigned char data[1024];
-   MD5_CTX md5;
-   MD5_Init(&md5);
-   int bytes;
-   while((bytes = fread(data,1,1024, file)) != 0){
-      MD5_Update(&md5, data , bytes);
 
-   }
-MD5_Final(result, &md5);
-fclose(file);
-char buf[33];
-
-for(int i = 0; i< MD5_DIGEST_LENGTH; i++){
-   sprintf(&buf[i*2], "%02x", result[i]);
-}
-buf[32] = '\0';
-
-printf("MD5 Checksum: %s\n", buf);
-
-}
-
-
-
+            crypto::SHA256 sha256;
+            std::string finalHash;
+            FILE *file = fopen(actualFilename.GetFullPath().c_str(), "rb");
+            if(file){
+               char data[1024];
+               while(size_t bytes = fread(data, 1, sizeof(data),file)){
+                  sha256.Update(data,bytes);
+               }
+               finalHash = sha256.Finalize();
+               fclose(file);
+               printf("SHA256 Checksum: %s\n", finalHash.c_str());
+            }
+                    
 
             }
             else
